d9519e775779cfe3da74825759b49dedd7396c1b
oncallejas <oncallejas@stefanini.com>
2022-10-18 15:38:02 -0600

    fix: fixing issue #1
    The function project.ListProjects() was being called at init func, now its only called on Run:

--- cmd/list_projects.go
+++ cmd/list_projects.go
@@ -19,10 +19,10 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		project.ListProjects()
 	},
 }
 
 func init() {
 	listCmd.AddCommand(list_projectsCmd)
-	project.ListProjects()
 }
