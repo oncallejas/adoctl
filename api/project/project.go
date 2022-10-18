b86a3b2bc34799894de2f1d7e7f96f316e576a01
oncallejas <oncallejas@stefanini.com>
2022-10-18 17:40:38 -0600

    feat: adding command to create a project

--- api/project/project.go
+++ api/project/project.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/cheynewallace/tabby"
@@ -55,4 +56,50 @@ func ListProjects() {
 			responseValue = nil
 		}
 	}
+}
+
+func CreateProject(projectName *string, projectDescription *string, projectSourceControlType string, projectProcessId string) {
+	config, err := api.LoadConfig("$HOME")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+	organizationUrl := config.ADO_URL
+	personalAccessToken := config.ADO_TOKEN
+
+	connection := azuredevops.NewPatConnection(organizationUrl, personalAccessToken)
+
+	ctx := context.Background()
+
+	coreClient, err := core.NewClient(ctx, connection)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	capabilities := map[string]map[string]string{
+		"versioncontrol": {
+			"sourceControlType": projectSourceControlType,
+		},
+		"processTemplate": {
+			"templateTypeId": projectProcessId,
+		},
+	}
+
+	teamProject := core.TeamProject{}
+	teamProject.Name = projectName
+	teamProject.Description = projectDescription
+	// *core.ProjectVisibility := (*core.ProjectVisibility)(projectVisibility)
+	// visibility = projectVisibility
+	// teamProject.Visibility = visibility
+	teamProject.Capabilities = &capabilities
+
+	projectArgs := core.QueueCreateProjectArgs{
+		ProjectToCreate: &teamProject}
+
+	responseValue, err := coreClient.QueueCreateProject(ctx, projectArgs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Project queue to be created. ", responseValue)
+
 }
--- cmd/create_project.go
+++ cmd/create_project.go
@@ -4,9 +4,9 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/oncallejas/adoctl/api/project"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // projectCmd represents the project command
@@ -20,7 +20,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("project called from create")
+		projectName := viper.GetString("project-name")
+		projectDescription := viper.GetString("project-description")
+		projectSourceControlType := viper.GetString("project-source-control-type")
+		projectProcessId := viper.GetString("project-process-id")
+
+		project.CreateProject(&projectName, &projectDescription, projectSourceControlType, projectProcessId)
 	},
 }
 
@@ -28,15 +33,27 @@ func init() {
 	createCmd.AddCommand(create_projectCmd)
 
 	// Flag for project's name
-	// create_projectCmd.Flags().StringP("project-name", "pn", "", "Name of the project to be created")
-	// viper.BindPFlag("project-name", create_projectCmd.Flags().Lookup("project-name"))
+	create_projectCmd.Flags().StringP("name", "n", "", "Name of the project to be created")
+	create_projectCmd.MarkFlagRequired("name")
+	viper.BindPFlag("project-name", create_projectCmd.Flags().Lookup("name"))
 
 	// Flag for project's description
-	// create_projectCmd.Flags().StringP("project-description", "pd", "", "Projects description to be created")
-	// viper.BindPFlag("project-description", createCmd.Flags().Lookup("project-description"))
+	create_projectCmd.Flags().StringP("description", "d", "", "Projects description to be created")
+	create_projectCmd.MarkFlagRequired("description")
+	viper.BindPFlag("project-description", create_projectCmd.Flags().Lookup("description"))
+
+	// Flag to set project's visibility
+	// create_projectCmd.Flags().StringP("visibility", "v", "private", "Visibility of the project")
+	// create_projectCmd.MarkFlagRequired("visibility")
+	// viper.BindPFlag("project-visibility", create_projectCmd.Flags().Lookup("visibility"))
+
+	// Flag to set the version control type of the project
+	create_projectCmd.Flags().StringP("source-control-type", "s", "", "Source control type of the project")
+	create_projectCmd.MarkFlagRequired("source-control-type")
+	viper.BindPFlag("project-source-control-type", create_projectCmd.Flags().Lookup("source-control-type"))
 
 	// Flag for process id to be relationated with project
-	// create_projectCmd.Flags().StringP("process-id", "pid", "", "Process Id to be used to create project")
-	// viper.BindPFlag("process-id", createCmd.Flags().Lookup("process-id"))
-
+	create_projectCmd.Flags().StringP("process-id", "i", "", "Process Id to be used to create project")
+	create_projectCmd.MarkFlagRequired("process-id")
+	viper.BindPFlag("project-process-id", create_projectCmd.Flags().Lookup("process-id"))
 }
